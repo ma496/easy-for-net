921a14652ba5a9fa17b4ffa65eb352688d466d82
Muhammad Ali <ma496@outlook.com>
2023-04-13 01:02:09 +0500

    delete react project

--- frontends/react-frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline w-full h-screen flex justify-center items-center">
-        Hello world!
-      </h1>
-    </div>
-  )
-}
