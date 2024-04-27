5b5ef71d48b4b82af781171624d29b56a8e4ca99
Muhammad Ali <ma496@outlook.com>
2024-04-27 15:27:49 +0500

    delete react project

--- frontends/react-frontend/app/(main)/administration/roles/page.tsx
+++
@@ -1,7 +0,0 @@
-const Roles = () => {
-  return (
-    <h1>Roles</h1>
-  )
-}
-
-export default Roles;
--- frontends/react-frontend/app/(main)/nested-menu-1/2/1/1/page.tsx
+++
@@ -1,7 +0,0 @@
-const NestedMenu1211 = () => {
-  return (
-    <h1>nested-menu-1/2/1/1</h1>
-  )
-}
-
-export default NestedMenu1211
