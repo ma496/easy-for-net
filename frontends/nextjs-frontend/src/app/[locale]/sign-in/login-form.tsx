7c39929dffb30161746a23b48ddb866e0d360c36
Muhammad Ali <ma496@outlook.com>
2024-06-26 13:00:32 +0500

    zod error localization

---
+++ frontends/nextjs-frontend/src/app/[locale]/sign-in/login-form.tsx
@@ -0,0 +1,65 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useTranslations } from "next-intl";
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { useI18nZodErrors } from '@/lib/useI18nZodErrors'
+
+const formSchema = z.object({
+  email: z.string().min(1),
+  password: z.string().min(6)
+})
+
+export function LoginForm() {
+  useI18nZodErrors()
+  const t = useTranslations()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{t('Email')}</FormLabel>
+              <FormControl>
+                <Input placeholder="m@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{t('Password')}</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">{t('Signin')}</Button>
+      </form>
+    </Form>
+  )
+}
